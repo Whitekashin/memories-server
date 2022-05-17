714d03e8feb3fcc3599b93796511cf681a69f1d6
Whitekashin <patrick.reitbauer@web.de>
2022-05-17 14:39:52 +0200

    New routes for Deleting, Liking a Post

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -13,11 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-
-const CONNECTION_URL = "mongodb+srv://mcfacestab:GH0STWR4TH!@cluster0.cj15x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
